d956d4d9b2b6698ca40e9dcf70c517ba39a58e29
musab <mbwedyan@gmail.com>
2020-12-16 13:22:22 +0200

    cla

---
+++ java.js
@@ -0,0 +1,56 @@
+
+var ahmadIsTheName = "son";
+var number = 10; 
+//alert(number);
+
+//if number is  equal to 10 then print the number else print The number is less that 10
+//if (number > 10 ) {
+   //     alert(number);
+   //  } else {
+      //   alert("The number is less that 10");
+   //  }
+
+
+
+
+
+//if (ahmadIsTheName == "son" ){
+     //    alert("he is my boy");
+  //  } else {
+   //      alert("he is not ");
+   // }
+
+
+
+  //  var jop = prompt("What is your jop?");
+  //  var position = prompt("What is your position?");
+
+   // checkjop(jop);
+   // checkPositon(positon);
+
+   // function checkjop(jop) {
+   //     alert("welcome "+jop);
+   // }
+    
+   // function checkPositon(position){
+      //  if (position == "site") {
+       //     alert("Logged in");
+      //  } else {
+      //      alert("you are not architect");
+     //   }
+  //  }
+   
+
+
+// take the distance of hight and width from the user 
+var width = prompt("Enter the width ");
+var hight = prompt("Enter the hight ");
+
+multi()
+
+function multi() {
+    var TotalArea = parseInt(width) * parseInt(hight);
+    alert(TotalArea);
+}
+
+
